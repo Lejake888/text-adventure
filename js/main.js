eafc4b21b68457f329e87a8ac4391f5e8933c6c8
Jake <lejake888@gmail.com>
2019-12-13 23:15:58 +0000

    Trying to clear log on button press, to display new message

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ let characterCount = 0;
 let text = "Welcome to the 'text-based adverture' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
 let speed = 50; 
 
-displayText = () => {
+const clearLog = () => {
+  document.getElementById("log").innerHTML = ""
+}
+
+const displayText = () => {
   if (characterCount < text.length) {
     document.getElementById("log").innerHTML += text.charAt(characterCount);
     characterCount++;
@@ -15,6 +19,10 @@ displayText = () => {
 }
 
 let nextButton = document.getElementById("button")
+
+nextButton.addEventListener = () => {
+  clearLog()
+}
 
 window.onload = () => {
     displayText()
