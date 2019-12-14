0be385a2da1249e1c65ab0b7735f39a739e890e4
Jake <lejake888@gmail.com>
2019-12-14 19:10:49 +0000

    Message disappears, working on loading next message

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 let characterCount = 0;
-let text = "Welcome to the 'text-based adverture' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
+let text = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
+let text2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
+
 let speed = 50; 
+
+const nextMessage = () => {
+  displayText(text2)
+}
 
 const clearLog = () => {
   document.getElementById("log").innerHTML = ""
+  nextMessage()
 }
 
-const displayText = () => {
+const displayText = (currentText) => {
   if (characterCount < text.length) {
     document.getElementById("log").innerHTML += text.charAt(characterCount);
     characterCount++;
@@ -14,16 +21,16 @@ const displayText = () => {
   }
   else {
     document.getElementById("button").style.visibility = "visible";
-
+    count = 0;
   }
 }
 
 let nextButton = document.getElementById("button")
 
-nextButton.addEventListener = () => {
+nextButton.addEventListener("click", () => {
   clearLog()
-}
+});
 
 window.onload = () => {
-    displayText()
+    displayText(text)
 }
