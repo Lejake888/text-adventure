5e05410f6626088d08ef63240cc4c230ba80e291
Jake <lejake888@gmail.com>
2019-12-14 19:20:33 +0000

    Successfully switches messages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let characterCount = 0;
 let text = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
 let text2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
-
+// Text will eventually be in an array, push messages
 let speed = 50; 
 
 const nextMessage = () => {
+  characterCount = 0
   displayText(text2)
 }
 
@@ -14,14 +15,13 @@ const clearLog = () => {
 }
 
 const displayText = (currentText) => {
-  if (characterCount < text.length) {
-    document.getElementById("log").innerHTML += text.charAt(characterCount);
+  if (characterCount < currentText.length) {
+    document.getElementById("log").innerHTML += currentText.charAt(characterCount);
     characterCount++;
-    setTimeout(displayText, speed);
+    setTimeout(() => {displayText(currentText)}, speed)
   }
   else {
     document.getElementById("button").style.visibility = "visible";
-    count = 0;
   }
 }
 
