33a5ad4008243f75d1eb906726383e063d234ea8
Jake <lejake888@gmail.com>
2019-12-15 22:30:07 +0000

    Messages stored in an array, messages are displayed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-let characterCount = 0;
 let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
 let message2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
-// Text will eventually be in an array, push messages
+let message3 = "This is the 3rd message"
+let messageArray = [message1, message2, message3]
 let speed = 50; 
+let characterCount = 0;
+let messageCounter = 0;
 
 const nextMessage = () => {
-  characterCount = 0
-  displayText(message2)
+  characterCount = 0;
+  messageCounter++
+  displayText(messageArray[messageCounter])
 }
 
 const clearLog = () => {
