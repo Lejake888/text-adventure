201a669e5570656dc9ebfc458af034645b054536
Jake <lejake888@gmail.com>
2019-12-15 20:29:18 +0000

    Button only appears after text finishes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let characterCount = 0;
-let text = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
-let text2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
+let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
+let message2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
 // Text will eventually be in an array, push messages
 let speed = 50; 
 
 const nextMessage = () => {
   characterCount = 0
-  displayText(text2)
+  displayText(message2)
 }
 
 const clearLog = () => {
@@ -15,6 +15,7 @@ const clearLog = () => {
 }
 
 const displayText = (currentText) => {
+  document.getElementById("button").style.visibility = "hidden";
   if (characterCount < currentText.length) {
     document.getElementById("log").innerHTML += currentText.charAt(characterCount);
     characterCount++;
@@ -32,5 +33,5 @@ nextButton.addEventListener("click", () => {
 });
 
 window.onload = () => {
-    displayText(text)
+    displayText(message1)
 }
