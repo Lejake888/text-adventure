4c8e9912653f07f0261b22d0f03a9aa8b9122f66
Jake <lejake888@gmail.com>
2019-12-15 22:51:29 +0000

    Added an input box

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path through the game. Will you be able to win?";
-let message2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis earum aperiam quis dolor, neque eius autem culpa id, aspernatur minus itaque dolores officiis necessitatibus non numquam cumque maxime magni! Possimus!";
-let message3 = "This is the 3rd message"
-let messageArray = [message1, message2, message3]
+let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path throughout the game. Will you be able to win? Press the 'Next' button to continue";
+let message2 = "If you are given a choice, you must press the button "
+let message3 = "You start off in a very dark room, with a single lightbulb hanging over your head. There are three items on the floor (1- Torch) (2- Bread) (3- Knife)";
+let message4 = "This is the 3rd message"
+let messageArray = [message1, message2, message3, message4]
 let speed = 50; 
 let characterCount = 0;
 let messageCounter = 0;
+
+class Items {
+  constructor(name) {
+    this.name = name;
+    this.inventorySpace = inventorySpace;
+  }
+}
 
 const nextMessage = () => {
   characterCount = 0;
@@ -19,6 +27,8 @@ const clearLog = () => {
 
 const displayText = (currentText) => {
   document.getElementById("button").style.visibility = "hidden";
+  document.getElementById("inputBox").style.visibility = "hidden";
+
   if (characterCount < currentText.length) {
     document.getElementById("log").innerHTML += currentText.charAt(characterCount);
     characterCount++;
@@ -26,6 +36,7 @@ const displayText = (currentText) => {
   }
   else {
     document.getElementById("button").style.visibility = "visible";
+    document.getElementById("inputBox").style.visibility = "visible";
   }
 }
 
