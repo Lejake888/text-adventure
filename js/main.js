c7a357074cb905917123775dbf8f50a5713da666
Jake Le <lejake888@gmail.com>
2019-12-16 10:28:34 +0000

    Minor changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path throughout the game. Will you be able to win? Press the 'Next' button to continue";
-let message2 = "If you are given a choice, you must input the number assigned to that item. For example, you will be given a list of items (1- Torch) (2- Bread) (3- Knife). If you want to pick up the 'Torch', you would input the number '1'. Each item takes up a space in your inventory. Bigger items take up more space. By default, you have an inventory size of 4";
+let message1 = "Welcome to the 'text-based adventure' game! This game will include a few puzzle to test you, as well as choices that will determine your path throughout the game. Will you be able to win? Press the 'Next' button to continue.";
+let message2 = "If you are given a choice, you must input the number assigned to that item. For example, you will be given a list of items (1- Torch) (2- Bread) (3- Knife). If you want to pick up the 'Torch', you would input the number '1'. Each item takes up a space in your inventory. Bigger items take up more space. By default, you have an inventory size of 4.";
 let message3 = "You start off in a very dark room, with a single lightbulb hanging over your head. There are three items on the floor (1- Torch) (2- Bread) (3- Knife)";
 let message4 = "This is the 4th message"
 let messageArray = [message1, message2, message3, message4]
@@ -28,20 +28,22 @@ class Items {
   }
 }
 
+let player = new Player()
+
 const collectItem = (choice) => {
-  if (choice == "1") {
+  if (choice == 1) {
     item = new Items("Torch", 1)
     item.pickedUp = true
     player.storage.push(item)
     console.log(item)
   }
-  else if (choice == "2") {
+  else if (choice == 2) {
     item = new Items("Bread", 1)
     item.pickedUp = true
     player.storage.push(item)
     console.log(item)
   }
-  else if (choice == "3") {
+  else if (choice == 3) {
     item = new Items("Knife", 1)
     item.pickedUp = true
     player.storage.push(item)
@@ -62,7 +64,7 @@ const clearLog = () => {
 }
 
 const displayText = (currentText) => {
-  document.getElementById("button").style.visibility = "hidden";
+  document.getElementById("nextButton").style.visibility = "hidden";
   document.getElementById("inputBox").style.visibility = "hidden";
 
   if (characterCount < currentText.length) {
@@ -75,17 +77,15 @@ const displayText = (currentText) => {
       document.getElementById("inputBox").style.visibility = "visible";
       console.log(player)
     }
-    document.getElementById("button").style.visibility = "visible";
+    document.getElementById("nextButton").style.visibility = "visible";
   }
 }
 
-let nextButton = document.getElementById("button")
+let nextButton = document.getElementById("nextButton")
 
 nextButton.addEventListener("click", () => {
   clearLog()
 });
-
-let player = new Player()
 
 inputButton.addEventListener("click", () => {
   collectItem(choice)
